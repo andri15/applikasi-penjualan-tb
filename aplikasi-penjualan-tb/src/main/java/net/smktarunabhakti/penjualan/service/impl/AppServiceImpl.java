63ede88c53813c5a8ccb43b883277daca0d495d7
encode21 <sony.surahman98@gmail.com>
2014-05-16 15:30:38 +0700

    Databasenya sudah berhasil .. Hore

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/service/impl/AppServiceImpl.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/service/impl/AppServiceImpl.java
@@ -1,50 +1,57 @@
 package net.smktarunabhakti.penjualan.service.impl;
 
-import java.awt.print.Pageable;
-import net.smktarunabhakti.penjualan.dao.BarangDao;
-import net.smktarunabhakti.penjualan.domain.Barang;
-import net.smktarunabhakti.penjualan.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-public class AppServiceImpl implements AppService{
-	
-	
-	
+import net.smktarunabhakti.penjualan.dao.BarangDao;
+import net.smktarunabhakti.penjualan.domain.Barang;
+import net.smktarunabhakti.penjualan.service.AppService;
+
+@SuppressWarnings("unchecked")
+@Service("appService")
+@Transactional
+
+public class AppServiceImpl implements AppService {
+
 	@Autowired
 	public BarangDao barangDao;
-
+	
 	public void simpanBarang(Barang b) {
 		barangDao.save(b);
 		
 	}
 
 	public void hapusBarang(Barang b) {
-		if(b == null  || b.getId()==null){
+		Barang nul = null;
+		if (b == nul || b.getId() == null) {
 			return;
 		}
 		barangDao.delete(b);
-		
 	}
+		
 
 	public Page<Barang> cariSemuaBarang(Pageable p) {
-		if(p == null){
-			p = (Pageable) new PageRequest(0, 20);
+		// TODO Auto-generated method stub
+		if (p == null) {
+			p = new PageRequest(0,20);
 		}
-		return (Page<Barang>) barangDao.findAll((Sort) p);
+		return barangDao.findAll(p);
 	}
 
 	public Barang cariBarangById(String id) {
-		if(!StringUtils.hasText(id)){
+		if (!StringUtils.hasText(id)) {
 			return null;
 		}
 		return barangDao.findOne(id);
 	}
 
-	public Long countSemuaBarang() {
+	public Long counSemuaBarang() {
+		// TODO Auto-generated method stub
 		return barangDao.count();
 	}
 	
